ae8b458f27dd2f7f087a4c57d6a70b76ddd9e149
Colin Mikulski <126927661+cmikul@users.noreply.github.com>
2025-08-16 01:49:50 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-const dev = process.env.NODE_ENV === 'development';
-const repoName = dev ? '' : '/my-site'; // replace with your repo name
-
 export default {
 	preprocess: preprocess({ typescript: false }),
 	kit: {
@@ -11,7 +8,7 @@ export default {
 			fallback: 'index.html'
 		}),
 		paths: {
-			base: repoName
+			base: '/my-site'
 		}
 	}
 };
