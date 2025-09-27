1293bed6f83f0e3ac2663e56479016aceed1056e
cmikul <cmmjake2@gmail.com>
2025-09-27 12:29:32 -0400

    Updates

---
+++ _app/immutable/nodes/2.Dynu-7Gf.js
@@ -0,0 +1,100 @@
+import"../chunks/DsnmJJEf.js";import{i as H}from"../chunks/D3t2W5AK.js";import{B as S,C as L,T as z,aj as ie,A as ne,E as re,M as oe,ak as le,al as ce,am as de,an as me,K as pe,J as F,I as V,c as fe,R as ue,Q as he,ao as ge,V as A,W as j,Y as R,Z as y,_ as P,a1 as f,u as C,y as N,a2 as M,$ as c,a0 as o,ah as B,a9 as ve,a as v,a8 as X,s as E,ap as xe,aq as K,ac as O,X as Q,ar as J,ad as be,as as ye}from"../chunks/CKowcPRT.js";import{b as _e,a as Z,s as Y,c as we,d as $,e as U,i as ee}from"../chunks/CehxxvZr.js";import{p as _,r as te,s as ke}from"../chunks/d8VDF-qM.js";function Se(u,e,n,a,d,x){let s=S;S&&L();var l,i,t=null;S&&z.nodeType===ie&&(t=z,L());var b=S?z:u,r;ne(()=>{const m=e()||null;var w=le;m!==l&&(r&&(m===null?ue(r,()=>{r=null,i=null}):m===i?he(r):ge(r)),m&&m!==i&&(r=oe(()=>{if(t=S?t:document.createElementNS(w,m),ce(t,t),a){S&&de(m)&&t.append(document.createComment(""));var g=S?me(t):t.appendChild(pe());S&&(g===null?F(!1):V(g)),a(t,g)}fe.nodes_end=t,b.before(t)})),l=m,l&&(i=l))},re),s&&(F(!0),V(b))}var je=j('<a class="p-4 sm:p-6 md:p-7 flex flex-col gap-4 rounded-lg border border-solid border-cyan-700 text-center group cursor-pointer hover:border-cyan-600 duration-200"><div class="bg-slate-950 grid place-items-center px-4 text-5xl md:text-6xl -mt-10 sm:-mt-12 md:-mt-14 lg:-mt-14 lg:-mt-16 mx-auto duration-200"><i></i></div> <h3 class="font-medium text-xl sm:text-2xl md:text-3xl"> </h3> <!> <div class="flex-1 flex justify-between gap-4 items-center"><div class="ml-auto cursor-pointer hover:text-slate-950 duration-200 relative after:absolute after:top-0 after:h-0 after:right-full after:w-full after:h-full after:bg-white after:duration-200 hover:after:translate-x-full after:z-[-1] overflow-hidden"><p class="relative z-4">Go to &rarr;</p></div></div></a>');function D(u,e){A(e,!1);let n=_(e,"step",8);H();var a=je(),d=c(a),x=c(d);o(d);var s=f(d,2),l=c(s,!0);o(s);var i=f(s,2);_e(i,e,"default",{}),B(2),o(a),R(t=>{Z(a,"href",(N(n()),C(()=>n().href))),Z(a,"target",t),Y(x,1,we((N(n()),C(()=>n().icon)))),M(l,(N(n()),C(()=>n().name)))},[()=>(N(n()),C(()=>n().href.startsWith("http")?"_blank":null))]),y(u,a),P()}var Ie=j('<span class="flytext svelte-184wj3z"> <span></span></span>');function Ce(u,e){A(e,!1);let n=_(e,"words",24,()=>["Biochemist","Mathematician","Data Scientist","Programmer"]),a=_(e,"typingSpeed",8,150),d=_(e,"deletingSpeed",8,100),x=_(e,"pauseDuration",8,1500),s=X(""),l=0,i=!1,t=X(!1);function b(){E(t,!1);const p=n()[l];let h=a();i?(E(s,p.substring(0,v(s).length-1)),h=d(),v(s)===""&&(i=!1,l=(l+1)%n().length)):(E(s,p.substring(0,v(s).length+1)),v(s)===p&&(E(t,!0),i=!0,h=x())),setTimeout(b,h)}ve(()=>{setTimeout(b,a())}),H();var r=Ie(),m=c(r),w=f(m);let g;o(r),R(p=>{M(m,`${v(s)??""} `),g=Y(w,1,"cursor !bg-cyan-400 svelte-184wj3z",null,g,p)},[()=>({blinking:v(t)})]),y(u,r),P()}/**
+ * @license @lucide/svelte v0.544.0 - ISC
+ *
+ * ISC License
+ * 
+ * Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2023 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2025.
+ * 
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * 
+ * ---
+ * 
+ * The MIT License (MIT) (for portions derived from Feather)
+ * 
+ * Copyright (c) 2013-2023 Cole Bemis
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */const Ne={xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor","stroke-width":2,"stroke-linecap":"round","stroke-linejoin":"round"};var Ee=xe("<svg><!><!></svg>");function Me(u,e){A(e,!0);const n=_(e,"color",3,"currentColor"),a=_(e,"size",3,24),d=_(e,"strokeWidth",3,2),x=_(e,"absoluteStrokeWidth",3,!1),s=_(e,"iconNode",19,()=>[]),l=te(e,["$$slots","$$events","$$legacy","name","color","size","strokeWidth","absoluteStrokeWidth","iconNode","children"]);var i=Ee();$(i,r=>({...Ne,...l,width:a(),height:a(),stroke:n(),"stroke-width":r,class:["lucide-icon lucide",e.name&&`lucide-${e.name}`,e.class]}),[()=>x()?Number(d())*24/Number(a()):d()]);var t=c(i);U(t,17,s,ee,(r,m)=>{var w=be(()=>ye(v(m),2));let g=()=>v(w)[0],p=()=>v(w)[1];var h=O(),k=Q(h);Se(k,g,!0,(I,W)=>{$(I,()=>({...p()}))}),y(r,h)});var b=f(t);K(b,()=>e.children??J),o(i),y(u,i),P()}function Ae(u,e){A(e,!0);/**
+ * @license @lucide/svelte v0.544.0 - ISC
+ *
+ * ISC License
+ *
+ * Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2023 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2025.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * ---
+ *
+ * The MIT License (MIT) (for portions derived from Feather)
+ *
+ * Copyright (c) 2013-2023 Cole Bemis
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */let n=te(e,["$$slots","$$events","$$legacy"]);const a=[["circle",{cx:"12",cy:"12",r:"1"}],["path",{d:"M20.2 20.2c2.04-2.03.02-7.36-4.5-11.9-4.54-4.52-9.87-6.54-11.9-4.5-2.04 2.03-.02 7.36 4.5 11.9 4.54 4.52 9.87 6.54 11.9 4.5Z"}],["path",{d:"M15.7 15.7c4.52-4.54 6.54-9.87 4.5-11.9-2.03-2.04-7.36-.02-11.9 4.5-4.52 4.54-6.54 9.87-4.5 11.9 2.03 2.04 7.36.02 11.9-4.5Z"}]];Me(u,ke({name:"atom"},()=>n,{get iconNode(){return a},children:(d,x)=>{var s=O(),l=Q(s);K(l,()=>e.children??J),y(d,s)},$$slots:{default:!0}})),P()}var Pe=j(`<p>Python tool for extracting information from long-read mRNA
+                    transcripts, capable of handling multiple and overlapping
+                    coding sequences (CDS), and well-suited for identifying
+                    untranslated regions for translatomics. Working on support
+                    for <i>de novo</i> CDS identification.</p>`),We=j(`<p>High-performance webcam-based eye tracker that matches or
+                    exceeds competing tools, leveraging deep learning techniques
+                    to accurately predict a subject's gaze location. I designed
+                    most of the data pipeline which converts raw videos of
+                    subjects into usable training data.</p>`),Te=j(`<p>2D browser-based gravity simulation created in p5.js
+                    featuring collision detection and mouse interaction.
+                    Parameter tuning enables dynamic and visually interesting
+                    particle behaviors, such as the coordination shown above.
+                    Stay tuned for a more interactive stand-alone version!</p>`),ze=j('<div class="flex gap-6 sm:gap-8"><p class="poppins text-4xl sm:text-5xl md:text-6xl text-slate-500 font-semibold"></p> <div class="flex flex-col gap-6 sm:gap-8"><h3 class="text-2xl sm:text-3xl md:text-5xl"> </h3> <p> </p></div></div>'),De=j(`<main class="flex flex-col flex-1 p-4"><section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14"><div class="flex flex-col text-left gap-6 md:gap-8"><h2 class="font-semibold text-3xl sm:text-4xl md:text-5xl"><span class="poppins text-cyan-400">Hi! My name is Colin Mikulski</span></h2> <h2 class="font-semibold text-2xl sm:text-3xl md:text-4xl">I am a <!></h2> <a class="inline-block md:hidden lg:hidden blueShadow mx-auto lg:mr-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3
+                group rounded-full bg-white text-slate-950 cursor-pointer" href="#contact"><div class="absolute top-0 right-full w-full h-full bg-cyan-400 opacity-20 group-hover:translate-x-full z-0 duration-200"></div> <h4 class="relative z-9">Get in touch &rarr;</h4></a></div> <div class="relative shadow-2xl grid place-items-center"></div></section> <section id="projects" class="scroll-mt-30 py-40 flex flex-col gap-10"><div class="flex flex-col gap-2 text-center"><h6 class="text-large sm:text-xl md:text-2xl">A few of my creative endeavors.</h6></div> <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6 lg:gap-8 text-sm"><!> <!> <!></div></section> <section id="about" class="scroll-mt-20 py-20 pt-0 lg:pt-16 lg:py-20 flex flex-col gap-10 sm:gap-10 md:gap-10 relative"><div class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-cyan-700 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-cyan-700 py-4"><h6 class="text-lg sm:text-xl md:text-2xl">Want to learn more?</h6> <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">A bit <span class="poppins text-cyan-400">about</span> me.</h3></div> <p class="mx-auto poppins font-semibold text-lg sm:text-xl md:text-2xl">I am...</p> <div class="flex flex-col gap-20 w-full mx-auto max-w-[800px]"></div></section></main>`);function Be(u){let e=[{name:"CDS Finder",icon:"fa-solid fa-dna",href:"https://github.com/Vassar-RiboLab/transcript-utils"},{name:"Eyetracker",icon:"fa-solid fa-eye",href:"https://github.com/jspsych/eyetracking-utils"},{name:"n-Body",icon:"fa-solid fa-circle",href:""},{name:"Kinetica",icon:Ae,href:""}],n=[{name:"a biochemist",description:"I have been captivated by biochemical systems since first learning about the central dogma of biology, which inspired me to pursue an undergraduate degree in biochemistry. Over time, this curiosity has evolved into a desire to use data science and mathematics to unravel complex biological problems, with the long-term vision of reducing disease and enhancing human health and lifespan."},{name:"a mathematician",description:"My journey as a mathematician began in my freshman year of college, sparked by a fascination with dynamical systems. Courses in linear algebra and differential equations opened a new lens for understanding physics and computer science. Motivated to deepen this perspective, I pursued a second major in mathematics, exploring nonlinear dynamics, numerical methods, and statistical modeling—tools that now empower me to tackle complex research challenges."},{name:"a data scientist",description:"I love exploring complex datasets and experimenting with the right mix of methods—whether simple regression, tree-based models like random forest or XGBoost, or transformer-based deep learning approaches. My focus is not just on prediction accuracy, but on uncovering the meaningful relationships hidden in data. I have trained models on high-performance clusters and automated hyperparameter tuning with tools like Weights & Biases. Recently, I applied random forest and XGBoost to reveal connections between mRNA structure and a translation initiation factor."},{name:"a developer",description:"I am passionate about programming and continuously refining my skills. I thrive on turning ideas into functional, tangible solutions, adapting quickly through online learning, experimentation, and hands-on practice. I particularly enjoy learning by building projects, such as this website, created with Svelte, or my n-Body simulation built in p5.js. In addition to these tools, I have extensive experience with Python, R, MATLAB, and SQL, and some experience with C/C++."}];var a=De(),d=c(a),x=c(d),s=f(c(x),2),l=f(c(s));Ce(l,{}),o(s),B(2),o(x),B(2),o(d);var i=f(d,2),t=f(c(i),2),b=c(t);D(b,{get step(){return e[0]},children:(p,h)=>{var k=Pe();y(p,k)},$$slots:{default:!0}});var r=f(b,2);D(r,{get step(){return e[1]},children:(p,h)=>{var k=We();y(p,k)},$$slots:{default:!0}});var m=f(r,2);D(m,{get step(){return e[2]},children:(p,h)=>{var k=Te();y(p,k)},$$slots:{default:!0}}),o(t),o(i);var w=f(i,2),g=f(c(w),4);U(g,5,()=>n,ee,(p,h,k)=>{var I=ze(),W=c(I);W.textContent=`0${k+1}`;var q=f(W,2),T=c(q),ae=c(T,!0);o(T);var G=f(T,2),se=c(G,!0);o(G),o(q),o(I),R(()=>{M(ae,v(h).name),M(se,v(h).description)}),y(p,I)}),o(g),o(w),o(a),y(u,a)}function Ve(u){Be(u)}export{Ve as component};
